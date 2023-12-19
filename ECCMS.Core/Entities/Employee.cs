69f82f417b380e591fc64cf73992a941dc8edbef
Madushanka Ishara <madushankaishara48@gmail.com>
2023-12-19 06:14:08 +0530

    Employee Branch Id Fix

--- ECCMS.Core/Entities/Employee.cs
+++ ECCMS.Core/Entities/Employee.cs
@@ -4,7 +4,7 @@ namespace ECCMS.Core.Entities;
 public class Employee : BaseEntity
 {
     public int UserId { get; set; }
-    public int BranchId { get; set; }
+    public int? BranchId { get; set; }
     public string Designation { get; set; } = null!;
     public int RoleId { get; set; }
     public bool? ChangePassword { get; set; }
